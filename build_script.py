0335a2f73e39baf06a2d260cd4303a5a877863f0
Adrian Budau <budau.adi@gmail.com>
2017-02-04 04:09:22 +0200

    Some changes to travis CI

--- build_script.py
+++ build_script.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-from os import system, environ
+from os import environ
+from subprocess import check_call
 
 version = environ.get('TRAVIS_RUST_VERSION')
 def run_command(command):
-    print(command)
-    system(command)
+    print(' '.join(command))
+    check_call(command)
 
-extra_args = ''
+extra_args = []
 if version == 'nightly':
-    extra_args = '--features clippy'
+    extra_args = ['--features', 'clippy']
 
-run_command('cargo build --verbose ' + extra_args)
-run_command('cargo test --verbose ' + extra_args)
+run_command(['cargo', 'build', '--verbose'] + extra_args)
+run_command(['cargo', 'test', '--verbose'] + extra_args)
 
 if version == 'nightly':
-    run_command('cargo bench --verbose ' + extra_args)
+    run_command(['cargo', 'bench', '--verbose'] + extra_args)
