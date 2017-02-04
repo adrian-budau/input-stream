e5e99682c8f9135db7b96cac642769c0a6a27603
Adrian Budau <budau.adi@gmail.com>
2017-02-05 01:24:33 +0200

    Added cargo deadlinks check for documentation links.
    And mac os x to travis CI + maybe get coverage working

--- build_script.py
+++ build_script.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 from os import environ
-from subprocess import check_call
+from subprocess import check_call, check_output, STDOUT
 
 version = environ.get('TRAVIS_RUST_VERSION')
-def run_command(command):
+def run_command(command, fail_on_output=False):
     print(' '.join(command))
-    check_call(command)
+    if not fail_on_output:
+        check_call(command)
+    else:
+        output = None
+        try:
+            output = check_output(command, stderr=STDOUT)
+            if len(output) > 0:
+                raise Exception("Expected no output")
+        finally:
+            print(output.decode('utf-8'))
 
 extra_args = []
 if version == 'nightly':
@@ -13,6 +22,8 @@ if version == 'nightly':
 
 run_command(['cargo', 'build', '--verbose'] + extra_args)
 run_command(['cargo', 'test', '--verbose'] + extra_args)
+run_command(['cargo', 'doc', '--verbose'])
+run_command(['cargo', 'deadlinks'], fail_on_output=True)
 
 if version == 'nightly':
     run_command(['cargo', 'bench', '--verbose'] + extra_args)
